f2c3fdbe4fdd435addf09bc0598e5d9d5b19399a
Morgan Conlan <morgan@pangr.ie>
2019-07-11 21:37:12 +0100

    Remove application

--- Slash/Slash.h
+++ Slash/Slash.h
@@ -2,9 +2,18 @@
 //  Slash.h
 //  Slash
 //
-//  Created by Chris Devereux on 30/01/2013.
-//  Copyright (c) 2013 ChrisDevereux. All rights reserved.
+//  Created by Morgan Conlan on 11/07/2019.
+//  Copyright © 2019 ChrisDevereux. All rights reserved.
 //
 
-#import <Slash/SLSMarkupParser.h>
-#import <Slash/SLSErrors.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for Slash.
+FOUNDATION_EXPORT double SlashVersionNumber;
+
+//! Project version string for Slash.
+FOUNDATION_EXPORT const unsigned char SlashVersionString[];
+
+#import "SLSMarkupParser.h"
+#import "SLSErrors.h"
+
--- SlashTests/SlashTests.m
+++ SlashTests/SlashTests.m
@@ -1,27 +1,25 @@
 //
-//  Tests_OSX.m
-//  Tests-OSX
+//  SlashTests.m
+//  SlashTests
 //
-//  Created by Chris Devereux on 20/01/2016.
-//  Copyright © 2016 ChrisDevereux. All rights reserved.
+//  Created by Morgan Conlan on 11/07/2019.
+//  Copyright © 2019 ChrisDevereux. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
 
-@interface Tests_OSX : XCTestCase
+@interface SlashTests : XCTestCase
 
 @end
 
-@implementation Tests_OSX
+@implementation SlashTests
 
 - (void)setUp {
-    [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
 }
 
 - (void)testExample {
